79b4cbe9030101a4151d47fbe56d79354e2a3708
Gioppyy <Giopanno006.com@gmail.com>
2025-01-02 00:49:20 +0100

    Maked some stuff
    - Code clean up
    - Added get last command
    - Added some code utilities

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,52 @@
-
 const app = document.querySelector("#app");
 const delay = ms => new Promise(res => setTimeout(res, ms));
-    
-    
-app.addEventListener("keypress", async function(event){
+const commands = [
+  ["all", "Show all commands."],
+  ["whoami", "Who I am and what do I do."],
+  ["project -a", "View all my public projects."],
+  ["social -a", "View all my social networks."],
+  ["clear", "Clear the screen."]
+];
+
+let last_command = "";
+
+app.addEventListener("keypress", async (event) => {
   if(event.key === "Enter"){
     await delay(150);
     getInputValue();
-   
+  
     removeInput();
     await delay(150);
     new_line();
   }
 });
 
-app.addEventListener("click", function(event){
+app.addEventListener("keydown", (e) => {
+  if (e.key === "ArrowUp") {
+    const input = document.querySelector("input");
+    input.value = last_command;
+    input.focus();
+  }
+});
+
+app.addEventListener("click", () => {
   const input = document.querySelector("input");
   input.focus();
-})
+});
 
+const getCommands = async (delay) => {
+  createText("List of commands:")
+  if (delay)
+    await delay(100);
+  
+  for (const cmd of commands) {
+    createCode(cmd[0], cmd[1]);
+    if (delay)
+      await delay(100);
+  }
+}
 
-async function open_terminal(){
+const open_terminal = async () => {
   createText("Loading all necessary libraries..")
   await delay(700);
   createText("Starting local server at http://127.0.0.1:8080/");
@@ -31,25 +57,12 @@ async function open_terminal(){
   await delay(400);
 
   createText("---------------------------");
-  createText("List of commands:")
-  await delay(100);
-  createCode("all", "Show all commands.");
-  await delay(100);
-  createCode("whoami", "Who i am and what do i do.");
-  await delay(100);
-  createCode("project -a", "View all my public projects.");
-  await delay(100);
-  createCode("social -a", "View all my social networks.");
-  await delay(100);
-  createCode("clear", "clear the screen.");
-  await delay(100);
+  getCommands(true);
 
   new_line();
 }
 
-
-function new_line(){
-  
+const new_line = () => {
   const p = document.createElement("p");
   const span1 = document.createElement("span");
   const span2 = document.createElement("span");
@@ -71,25 +84,15 @@ function new_line(){
   input.focus();
 }
 
-function removeInput(){
-  const div = document.querySelector(".type");
-  app.removeChild(div);
-}
-
-async function getInputValue(){
-  
+const getInputValue = async () => {
   const value = document.querySelector("input").value.trim();
+  last_command = value;
   if(value === "all"){
-    trueValue(value);
-    createText("List of commands:")
-    createCode("all", "Show all commands.");
-    createCode("whoami", "Who i am and what do i do.");
-    createCode("project -a", "View all my public projects.");
-    createCode("social -a", "View all my social networks.");
-    createCode("clear", "clear the screen.");
+    commandResponse(true, value);
+    getCommands(false);
   }
   else if(value === "whoami"){
-    trueValue(value);
+    commandResponse(true, value);
     createText("Hello! I'm <span class='green'>Michele</span> aka Gioppy.")
     createText("I'm <span class='green'>18 years</span> old and I have been a developer <span class='green'>since 2018</span>. \
       <br />I have started with small projects using the <a href='https://hub.spigotmc.org/javadocs/bukkit/' target='_blank' class='green'>Bukkit</a> API to develop Minecraft plugins. \
@@ -103,25 +106,27 @@ async function getInputValue(){
       <br /><br />I speak two languages fluently: <span class='green'>Italian</span> and <span class='green'>English</span>. I also have <span class='green'>conversational</span> proficiency in French, Spanish, and Arabic.")
   }
   else if(value === "project -a"){
-    trueValue(value);
-    createText("Take a view of my public projects on my \
+    commandResponse(true, value);
+    createProject("https://github.com/Gioppyy/SpaceInvader", "SpaceInvader", "A simple <span class='green'>Space Invader</span> developed with \
+      <a href='https://github.com/fondinfo/fondinfo/blob/master/g2d.py' target='_blank'><span class='green'>g2d</span> </a> for an UNIPR's python course");
+    createText("Take a view of the others public projects on my \
       <span class='green'> \
-      <a href='https://github.com/Gioppyy' target='_blank' class='green'> \
-      <i class='fab fa-github blue'></i> GitHub</a> \
-      </span>")
+        <a href='https://github.com/Gioppyy' target='_blank' class='green'> \
+        <i class='fab fa-github blue'></i> GitHub</a> \
+        </span>");
   }
   else if(value === "project"){
-    falseValue(value);
+    commandResponse(false, value);
     createText("Do you mean projects -a?")
   }
   else if(value === "social -a"){
-    trueValue(value);
+    commandResponse(true, value);
     createText("<a href='https://discord.gg' target='_blank'><i class='fab fa-discord'></i> Add me on Discord: <span class='green'>gioppy.</span></a>")
     createText("<a href='https://github.com/gioppyy' target='_blank'><i class='fab fa-github'></i> Follow me on github: <span class='green'>github.com/gioppyy</span></a>")
     createText("<a href='https://www.instagram.com/michele._.cava/' target='_blank'><i class='fab fa-instagram'></i> Follow me on Instagram: <span class='green'>instagram.com/michele._.cava</span></a>")
   }
   else if(value === "social"){
-    falseValue(value);
+    commandResponse(false, value);
     createText("Do you mean social -a?")
   }
   else if(value === "clear" || value === "cls"){
@@ -129,58 +134,64 @@ async function getInputValue(){
     document.querySelectorAll("section").forEach(e => e.parentNode.removeChild(e));
   }
   else{
-    falseValue(value);
+    commandResponse(false, value);
     createErrorText(`command not found: ${value}`)
   }
 }
 
-function trueValue(value){
-  
+const commandResponse = (correct, command) => {
   const div = document.createElement("section");
-  div.setAttribute("class", "type2")
-  const i = document.createElement("i");
-  i.setAttribute("class", "fas fa-angle-right icone")
   const mensagem = document.createElement("h2");
-  mensagem.setAttribute("class", "sucess")
-  mensagem.textContent = `${value}`;
+  const i = document.createElement("i");
+
+  div.setAttribute("class", "type2")
+
+  if (correct) {
+    i.setAttribute("class", "fas fa-angle-right icone")
+    mensagem.setAttribute("class", "sucess")
+  } else {
+    i.setAttribute("class", "fas fa-angle-right icone error")
+    mensagem.setAttribute("class", "error")
+  }
+
+  mensagem.textContent = `${command}`;
   div.appendChild(i);
   div.appendChild(mensagem);
   app.appendChild(div);
 }
 
-function falseValue(value){
-  
-  const div = document.createElement("section");
-  div.setAttribute("class", "type2")
-  const i = document.createElement("i");
-  i.setAttribute("class", "fas fa-angle-right icone error")
-  const mensagem = document.createElement("h2");
-  mensagem.setAttribute("class", "error")
-  mensagem.textContent = `${value}`;
-  div.appendChild(i);
-  div.appendChild(mensagem);
-  app.appendChild(div);
+const createProject = (link, title, text) => {
+  const a = document.createElement("a");
+
+  a.href = link;
+  a.target = "_blank";
+  a.innerHTML =
+    `<p class="code">${title}</p> <p><span class='text'> ${text} </span></p>`;
+
+  app.appendChild(a);
 }
 
-function createText(text, classname) {
+const createCode = (code, text) => {
   const p = document.createElement("p");
-  
+  p.setAttribute("class", "code");
+  p.innerHTML =
+    `${code} <br/><span class='text'> ${text} </span>`;
+  app.appendChild(p);
+}
+
+const createText = (text) => {
+  const p = document.createElement("p");
   p.innerHTML = text;
   app.appendChild(p);
 }
 
-function createErrorText(text) {
+const createErrorText = (text) => {
   const p = document.createElement("p");
   p.innerText = text;
   app.appendChild(p);
 }
 
-function createCode(code, text){
-  const p = document.createElement("p");
-  p.setAttribute("class", "code");
-  p.innerHTML =
-  `${code} <br/><span class='text'> ${text} </span>`;
-  app.appendChild(p);
+const removeInput = () => {
+  const div = document.querySelector(".type");
+  app.removeChild(div);
 }
-
-open_terminal();
